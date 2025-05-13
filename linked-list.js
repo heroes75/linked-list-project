ceb6c184ea6c7579e8753c2af630cd9ae7f71c6e
heroes75 <123456789+odin@users.noreply.github.com>
2025-05-13 18:16:47 +0000

    stuck with prepend() method

--- linked-list.js
+++ linked-list.js
@@ -1,7 +1,7 @@
 class Node {
-    constructor(value) {
+    constructor(value = null, next = null) {
         this.value= value;
-        this.next  = null
+        this.next  = next
     }
     
 }
@@ -12,20 +12,42 @@ class LinkedList {
         this.next
     }
     append(value) {
+        
         let node = new Node(value);
-        let nextEnd = this.next
+
         if (this.value === undefined && this.next === undefined) {
+            let node = new Node(value);
             this.value = node.value;
             this.next = node.next;
             return
-        } 
-        console.log({nextEnd});
-        while (nextEnd !== null) {
-            nextEnd = this.next.next;
         }
-        nextEnd = node;
-        console.log(nextEnd);
+
+        let head = this;
+        console.log(head);
         
+        while(head.next !== null) {
+            head = head.next;
+        }
+
+        head.next = node;
+        
+    }
+    prepend(value) {
+        let head = this;
+        let node = new Node(value, head);
+        console.log("before", head);
+        
+        //head = { value: node.value, next: this};
+        console.log({head});
+        
+        //head.value = node.value;
+        //try with this in start to end
+        this.head = node;
+        //node.next = head;
+        //head = new Node(value, head);
+        //head.next = head
+        //head.value = node.value;
+        console.log("end", head);
     }
 }
 
@@ -34,6 +56,12 @@ list.append("value");
 console.log(list);
 list.append("value1");
 console.log(list);
-list.append("value2");
+//console.log(list.next);
+//list.append("value2");
 console.log(list);
+//list.append("value3");
+//list.append("value4");
+//list.append("value5");
+list.prepend("value0")
 
+console.log(list)
